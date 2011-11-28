a447b4323bf769634218e1a58e4e14b7a68981a3
rarnu <rarnu1985@gmail.com>
2011-11-28 23:16:29 +0800

    upload D23HSwitcher and the starting of refact

---
+++ D23HSwitcher/src/com/snda/root/d23h/root/CommandResult.java
@@ -0,0 +1,7 @@
+package com.snda.root.d23h.root;
+
+public class CommandResult {
+
+	public String result = "";
+	public String error = "";
+}
---
+++ RarnuAndroidRoot/src/com/snda/root/CommandResult.java
@@ -0,0 +1,7 @@
+package com.snda.root;
+
+public class CommandResult {
+
+	public String result = "";
+	public String error = "";
+}
