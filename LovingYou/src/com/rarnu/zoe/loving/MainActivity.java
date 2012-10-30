d7a4d0147788be51d6532835bdba13e4dc81cfce
rarnu <rarnu1985@gmail.com>
2012-10-30 21:03:15 +0800

    upload loving you the public welfare project

---
+++ LovingYou/src/com/rarnu/zoe/loving/MainActivity.java
@@ -0,0 +1,14 @@
+package com.rarnu.zoe.loving;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+}
