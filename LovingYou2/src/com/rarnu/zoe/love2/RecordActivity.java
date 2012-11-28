1d43be701561de42985ea4f7e583be84727094ef
nick <nickhe@anjukeinc.com>
2012-11-28 18:21:04 +0800

    record activity half complete

--- LovingYou2/src/com/rarnu/zoe/love2/RecordActivity.java
+++ LovingYou2/src/com/rarnu/zoe/love2/RecordActivity.java
@@ -1,12 +1,26 @@
 package com.rarnu.zoe.love2;
 
+import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.rarnu.zoe.love2.base.BaseActivity;
 import com.rarnu.zoe.love2.comp.Title;
 
 public class RecordActivity extends BaseActivity implements OnClickListener {
+
+	TextView tvRecordDesc;
+	RelativeLayout layLines;
+	String[] text = null;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		text = getResources().getStringArray(R.array.record_text);
+		tvRecordDesc.setText(text[0]);
+	}
 
 	@Override
 	protected void setContentView() {
@@ -19,10 +33,14 @@ public class RecordActivity extends BaseActivity implements OnClickListener {
 		super.initComponents();
 
 		title.getBarItem(Title.BARITEM_CENTER).setText(R.string.record_today);
-		title.getBarItem(Title.BARITEM_RIGHT).setIcon(R.drawable.record_history);
+		title.getBarItem(Title.BARITEM_RIGHT)
+				.setIcon(R.drawable.record_history);
 		title.getBarItem(Title.BARITEM_LEFT).setIcon(R.drawable.home);
+
+		tvRecordDesc = (TextView) findViewById(R.id.tvRecordDesc);
+		layLines = (RelativeLayout) findViewById(R.id.layLines);
 	}
-	
+
 	@Override
 	protected void initEvents() {
 		super.initEvents();
@@ -39,6 +57,6 @@ public class RecordActivity extends BaseActivity implements OnClickListener {
 		case Title.ITEM_RIGHT:
 			break;
 		}
-		
+
 	}
 }
