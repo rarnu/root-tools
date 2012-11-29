3059dc9ecab6d2ebbececb1b1e2f1949bb88f714
nick <nickhe@anjukeinc.com>
2012-11-29 16:16:08 +0800

    hals history activity UI

--- LovingYou2/src/com/rarnu/zoe/love2/HistoryActivity.java
+++ LovingYou2/src/com/rarnu/zoe/love2/HistoryActivity.java
@@ -3,15 +3,24 @@ package com.rarnu.zoe.love2;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.GridView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.rarnu.zoe.love2.base.BaseActivity;
 import com.rarnu.zoe.love2.comp.Title;
 
 public class HistoryActivity extends BaseActivity implements OnClickListener {
 
+	RelativeLayout layTree;
+	TextView tvTree;
+	TextView tvValue5, tvValue4, tvValue3, tvValue2, tvValue1;
+	GridView gvHistory;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		buildTree(Global.database.getDay());
 	}
 
 	@Override
@@ -24,6 +33,16 @@ public class HistoryActivity extends BaseActivity implements OnClickListener {
 		super.initComponents();
 		title.getBarItem(Title.BARITEM_CENTER).setText(R.string.history);
 		title.getBarItem(Title.BARITEM_LEFT).setIcon(R.drawable.home);
+
+		layTree = (RelativeLayout) findViewById(R.id.layTree);
+		tvTree = (TextView) findViewById(R.id.tvTree);
+		tvValue5 = (TextView) findViewById(R.id.tvValue5);
+		tvValue4 = (TextView) findViewById(R.id.tvValue4);
+		tvValue3 = (TextView) findViewById(R.id.tvValue3);
+		tvValue2 = (TextView) findViewById(R.id.tvValue2);
+		tvValue1 = (TextView) findViewById(R.id.tvValue1);
+		gvHistory = (GridView) findViewById(R.id.gvHistory);
+
 	}
 
 	@Override
@@ -39,7 +58,11 @@ public class HistoryActivity extends BaseActivity implements OnClickListener {
 			finish();
 			break;
 		}
-		
+
+	}
+	
+	private void buildTree(int day) {
+		// TODO: build tree
 	}
 
 }
