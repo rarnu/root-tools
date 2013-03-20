6fbafbaf8a51c3898f3a6790cd59626856133713
Nick <rarnu1985@gmail.com>
2013-03-20 11:37:27 +0800

    add a method for the devices has the second SDCard

--- PackageParser4/src/com/rarnu/tools/root/utils/MiscUtils.java
+++ PackageParser4/src/com/rarnu/tools/root/utils/MiscUtils.java
@@ -7,6 +7,8 @@ import android.os.Environment;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.rarnu.command.CommandResult;
+import com.rarnu.command.RootUtils;
 import com.rarnu.root.pp4.R;
 
 public class MiscUtils {
@@ -20,5 +22,32 @@ public class MiscUtils {
 				.getAbsolutePath());
 		Toast.makeText(context, R.string.scan, Toast.LENGTH_LONG).show();
 	}
+	
+	public static String getSecondSdcardPath(boolean hasSplit) {
+		CommandResult ret = RootUtils.runCommand("mount", false);
+		String[] lines = ret.result.split("\n");
 
+		String systemSdcard = Environment.getExternalStorageDirectory()
+				.getAbsolutePath();
+		String extSdcard = "";
+
+		for (String s : lines) {
+			if (s.contains("secure") || s.contains("asec")) {
+				continue;
+			}
+			if (s.contains("fat") || s.contains("fuse")) {
+				String columns[] = s.split(" ");
+				if (columns != null && columns.length > 3) {
+					if (columns[3].contains("rw,")) {
+						extSdcard += columns[1] + "\n";
+					}
+				}
+			}
+		}
+		extSdcard = extSdcard.replace(systemSdcard, "").trim();
+		if (!extSdcard.endsWith("/") && hasSplit) {
+			extSdcard += "/";
+		}
+		return extSdcard;
+	}
 }
