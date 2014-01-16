1005c3cffb0255ca80df84f7f92d4ba78d044644
rarnu <rarnu1985@gmail.com>
2014-01-16 21:53:21 +0800

    fix the broken file state when the google apk is deleted

--- PackageParser4/src/com/rarnu/tools/root/utils/GoogleUtils.java
+++ PackageParser4/src/com/rarnu/tools/root/utils/GoogleUtils.java
@@ -91,8 +91,9 @@ public class GoogleUtils {
                         newFilePath = DirHelper.GOOGLE_DIR + sdkVer + "/" + item.path + "/" + item.fileName;
                         break;
                 }
-                ret = ApkUtils.checkSignature(filePath, newFilePath) ? 0 : 2;
-
+                if (new File(newFilePath).exists()) {
+                    ret = ApkUtils.checkSignature(filePath, newFilePath) ? 0 : 2;
+                }
             } else {
                 ret = 0;
             }
