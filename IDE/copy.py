07fdac76264e7d4bade19ea0d313fe1819c2caa8
rarnu <rarnu1985@gmail.com>
2014-10-21 10:49:27 +0800

    * refact some script

--- IDE/copy.py
+++ IDE/copy.py
@@ -5,61 +5,44 @@ from platform import uname as uname
 
 class JetBrainsConfig:
 
-    __home = ""
-    __path_idea = ""
-    __path_pycharm = ""
-    __path_webstorm = ""
-    __path_phpstorm = ""
+    __home__ = ""
 
-    __path_color = "/colors/"
-    __path_codestyle = "/codestyles/"
-    __path_keymap = "/keymaps/"
+    __base__ = ["/config", "/Library/Preferences"]
+    __apps__ = None
+    __apps_linux__ = ["/.IntelliJIdea13", "/.PyCharm30", "/.WebStorm8", "/.WebIde70"]
+    __apps_mac__ = ["/IntelliJIdea13", "/PyCharm30", "/WebStorm8", "/WebIde70"]
 
-    __file_color = "rarnu_color.icls"
-    __file_codestyle = "rarnu_codestyle.xml"
-    __file_keymap = "rarnu_keymap.xml"
+    __paths__ = ["/colors/", "/codestyles/", "/keymaps/"]
+    __files__ = ["rarnu_color.icls", "rarnu_codestyle.xml", "rarnu_keymap.xml"]
 
     __supported = True
 
     def __init__(self):
-        self.__home = osenv("HOME")
-        os_name = uname()[0].lower()
-        if os_name.lower() == "linux":
-            path_config = "/config"
-            self.__path_idea = "/.IntelliJIdea13" + path_config
-            self.__path_pycharm = "/.PyCharm30" + path_config
-            self.__path_webstorm = "/.WebStorm8" + path_config
-            self.__path_phpstorm = "/.WebIde70" + path_config
-            self.__supported = True
-        elif os_name.lower() == "darwin":
-            path_pref = "/Library/Preferences"
-            self.__path_idea = path_pref + "/IntelliJIdea13"
-            self.__path_pycharm = path_pref + "/PyCharm30"
-            self.__path_webstorm = path_pref + "/WebStorm8"
-            self.__path_phpstorm = path_pref + "/WebIde70"
-            self.__supported = True
+        self.__home__ = osenv("HOME")
+        __os__ = uname()[0].lower()
+        self.__supported = (__os__ == "linux" or __os__ == "darwin")
+        if __os__ == "linux":
+            self.__apps__ = self.__apps_linux__
         else:
-            self.__supported = False
+            self.__apps__ = self.__apps_mac__
 
-    def __copy_file(self, path):
-        color_path = path + self.__path_color
-        codestyle_path = path + self.__path_codestyle
-        keymap_path = path + self.__path_keymap
-        if pathexists(color_path):
-            shcopy(self.__file_color, color_path + self.__file_color)
-        if pathexists(codestyle_path):
-            shcopy(self.__file_codestyle, codestyle_path + self.__file_codestyle)
-        if pathexists(keymap_path):
-            shcopy(self.__file_keymap, keymap_path + self.__file_keymap)
+    def __copy_file(self):
+
+        for base in self.__base__:
+            for os_path in self.__apps__:
+                __path = self.__home__ + base + os_path
+
+                if pathexists(__path):
+                    for i in range(0, 3):
+                        file_path = __path + self.__paths__[i] + self.__files__[i]
+                        shcopy(self.__files__[i], file_path)
+                        print(file_path)
 
     def execute(self):
         if not self.__supported:
             print("os not supported.")
         else:
-            self.__copy_file(self.__home + self.__path_idea)
-            self.__copy_file(self.__home + self.__path_pycharm)
-            self.__copy_file(self.__home + self.__path_webstorm)
-            self.__copy_file(self.__home + self.__path_phpstorm)
+            self.__copy_file()
             print("done")
         pass
 
