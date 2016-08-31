62254fd707835caa8f21d008e7197a68ec678929
rarnu <hexiaojie@hujiang.com>
2016-08-31 11:03:45 +0800

    * add new project

---
+++ RootTools2/src/com/rarnu/tools/root/neo/MainActivity.java
@@ -0,0 +1,15 @@
+package com.rarnu.tools.root.neo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
