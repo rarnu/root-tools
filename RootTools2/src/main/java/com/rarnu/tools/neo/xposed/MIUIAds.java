baa0f4c2637c6b93794763d2c2b369b06b4a3131
rarnu <hexiaojie@hujiang.com>
2016-10-14 11:28:56 +0800

    * fuck ads in calendar and browser

--- RootTools2/src/main/java/com/rarnu/tools/neo/xposed/MIUIAds.java
+++ RootTools2/src/main/java/com/rarnu/tools/neo/xposed/MIUIAds.java
@@ -13,6 +13,7 @@ import com.rarnu.tools.neo.utils.ComponentUtils;
 import de.robv.android.xposed.*;
 import de.robv.android.xposed.callbacks.XC_InitPackageResources;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
+import org.json.JSONObject;
 
 import javax.net.ssl.SSLPeerUnverifiedException;
 import java.lang.annotation.Target;
@@ -232,6 +233,32 @@ public class MIUIAds implements IXposedHookLoadPackage, IXposedHookInitPackageRe
             XpUtils.findAndHookMethod("com.miui.calendar.ad.AdService", loadPackageParam.classLoader, "onHandleIntent", Intent.class, XC_MethodReplacement.returnConstant(null));
             XpUtils.findAndHookMethod("com.miui.calendar.card.single.local.SummarySingleCard", loadPackageParam.classLoader, "needShowAdBanner", XC_MethodReplacement.returnConstant(false));
             XpUtils.findAndHookMethod("com.miui.calendar.card.single.custom.ad.AdSingleCard", loadPackageParam.classLoader, "needDisplay", XC_MethodReplacement.returnConstant(false));
+            XpUtils.findAndHookMethod("com.miui.calendar.ad.AdUtils", loadPackageParam.classLoader, "getAdConfigJson", Context.class, XC_MethodReplacement.returnConstant(null));
+            XpUtils.findAndHookMethod("com.miui.calendar.card.single.custom.RecommendSingleCard", loadPackageParam.classLoader, "needDisplay", XC_MethodReplacement.returnConstant(false));
+            XpUtils.findAndHookMethod("com.miui.calendar.card.single.custom.ad.LargeImageAdSingleCard", loadPackageParam.classLoader, "needDisplay", XC_MethodReplacement.returnConstant(false));
+            Class<?> clsC = XpUtils.findClass(loadPackageParam.classLoader, "com.xiaomi.ad.internal.common.module.a$c");
+            if (clsC != null) {
+                XpUtils.findAndHookMethod("com.xiaomi.ad.internal.common.module.a", loadPackageParam.classLoader, "b", clsC, XC_MethodReplacement.returnConstant(null));
+            }
+            XpUtils.findAndHookMethod("com.xiaomi.ad.common.pojo.Ad", loadPackageParam.classLoader, "parseJson", JSONObject.class, XC_MethodReplacement.returnConstant(null));
+            XpUtils.findAndHookMethod("com.xiaomi.ad.internal.a.e", loadPackageParam.classLoader, "onAdInfo", String.class, XC_MethodReplacement.returnConstant(null));
+            XpUtils.findAndHookMethod("com.miui.calendar.util.DiskStringCache", loadPackageParam.classLoader, "getString", Context.class, String.class, new XC_MethodHook() {
+                @Override
+                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                    String key = (String) param.args[1];
+
+                    if (key.startsWith("bottom_banner_is_closed_today")) {
+                        param.setResult("true");
+                    }
+                }
+                @Override
+                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                    String key = (String) param.args[1];
+                    if (key.startsWith("bottom_banner_is_closed_today")) {
+                        param.setResult("true");
+                    }
+                }
+            });
             return;
         }
 
