229d43a7effa4995d9bc555e6bb1a7f152bbe0c1
rarnu <hexiaojie@hujiang.com>
2016-12-06 23:53:03 +0800

    * fixed Fuck ads in settings

--- README.md
+++ README.md
@@ -5,16 +5,20 @@ RootTools·Neo
 
 * RootTools 是一款专注于给 root 后的用户提供方便的软件。主要提供：
 	* ```应用冻结``` 不删除系统内的应用，而是将其冻结，在需要时可以解冻，但是别乱来哦，冻错了会无法开机，只能双清；
-	* ```自动化组件管理``` 一个应用内往往包含大量的『四大组件』并且持续耗电，这个功能可以帮助禁用组件，但是最大程度上不影响应用的正常使用；
+	* ```组件管理``` 一个应用内往往包含大量的『四大组件』并且持续耗电，这个功能可以帮助禁用组件，但是最大程度上不影响应用的正常使用；
 	* ```深度清理系统``` 运行久了，总会有各种各样的缓存，特别是 ART 缓存很难清理，留着会造成一些奇怪的问题，清理 ART 事不宜迟；
 	* ```深度清理内存``` 一键杀掉各种疑难杂症的后台应用，尽可能的释放内存，让手机回到刚开机时的状态。
 	
 * 同时也针对目前海量用户的 MIUI 系统进行相应的优化：
 	* ```去除系统广告``` 不知从何时起，MIUI 充斥着广告，这个功能将最大限度的帮助清理掉广告，以及各种带广告的闪屏页；
+        * 针对广告的问题，在软件内开设了反馈的入口，当您发现了广告时，可以截图并通过该入口进行反馈；
+        * 去广告同时也需要 root 权限，因为会向 hosts 写入数据，而 MIUI 要实现写 hosts 还必须解除 /system 分区的锁，解锁方法请自行搜索；
 	* ```去除通知栏搜索和负一屏``` 反正对于不常用的东西就是没啥好感，能够去掉的话就去掉吧；
 	* ```root 25 秒等待``` 又一个反人类设计，说好的为发烧而生呢，只能用一些黑科技来去掉这该死的 25 秒了，是谁说的时间就是金钱。
 	
 * 为了实现如去除广告等功能，必须借助 xposed 框架，以实现对目标方法的 Hook 和修改。因此要求用户的手机已安装 xposed 框架，请特别注意 MIUI 需要专用的 xposed，请自行搜索。
+
+* 本软件会跟随 MIUI 版本更新而更新，尽可能让用户的手机保持在一个干净无广告的状态下，平均的发版本频率为每周一次。
 
 - - -
 
@@ -25,7 +29,7 @@ RootTools·Neo
 
 * 编译软件
 
-	* 系统必备 jdk8 和 gradle 2.14.1
+	* 系统必备 jdk8 和 gradle 2.14.1 以上
 	
 	```
 	$ git clone https://github.com/rarnu/root-tools
@@ -41,3 +45,16 @@ RootTools·Neo
 	```
 	
 	* 此时即可以得到工作于 arm, mips, x86 三个平台的 libcmd.so                                                                                                                                                                 
+
+- - -
+
+* 特别感谢
+
+	| Icon | Name | Desc | Github |
+	| :--: | :-- | :-- | :-- | 
+	| <img src="http://diy.ourocg.cn/root/head/soulhacker.png" width="48" height="48"> | Soulhacker | 忠实的支持者和指导者，给予了大量的帮助 | [Github](https://github.com/soulhacker) |
+	| <img src="http://diy.ourocg.cn/root/head/coderstory.png" width="48" height="48"> | CoderStory | MiKit 作者，调教 MIUI 的先驱者 | [Github](https://github.com/coderstory) |
+	| <img src="http://diy.ourocg.cn/root/head/maomao.png" width="48" height="48"> | 猫猫姐姐 | 程序媛，OneCat 创始人 | [Github](https://github.com/chaojimiaomiao) |
+	| <img src="http://diy.ourocg.cn/root/head/laiga.png" width="48" height="48"> | 來嘠 | 帮助修复了 root 问题的玩机爱好者 | |
+	| <img src="http://diy.ourocg.cn/root/head/lanyuyinxue.png" width="48" height="48"> | 蓝羽映雪 | 帮助修复了浏览器广告的玩机爱好者 | |
+	
--- RootTools2/src/main/kotlin/com/rarnu/tools/neo/xposed/ads/FuckSettings.kt
+++ RootTools2/src/main/kotlin/com/rarnu/tools/neo/xposed/ads/FuckSettings.kt
@@ -1,6 +1,8 @@
 package com.rarnu.tools.neo.xposed.ads
 
+import android.app.Activity
 import android.os.Bundle
+import android.preference.PreferenceActivity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -17,6 +19,28 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage
 object FuckSettings {
     fun fuckSettings(loadPackageParam: XC_LoadPackage.LoadPackageParam) {
 
+        // make developer settings always visible
+        XpUtils.findAndHookMethod("com.android.settings.MiuiSettings", loadPackageParam.classLoader, "onCreate", Bundle::class.java, object : XC_MethodHook() {
+            @Throws(Throwable::class)
+            override fun afterHookedMethod(param: MethodHookParam) {
+                val a = param.thisObject as Activity
+                a.getSharedPreferences("development", 0).edit().putBoolean("show", true).apply()
+            }
+        })
+
+        val clsU = XpUtils.findClass(loadPackageParam.classLoader, "com.android.settings.device.u")
+        if (clsU != null && clsU.superclass.name == "com.android.settings.SettingsPreferenceFragment") {
+            XpUtils.findAndHookMethod("com.android.settings.MiuiSettings", loadPackageParam.classLoader, "onHeaderClick", PreferenceActivity.Header::class.java, Integer.TYPE, object : XC_MethodHook() {
+                @Throws(Throwable::class)
+                override fun beforeHookedMethod(param: MethodHookParam) {
+                    val h = param.args[0] as PreferenceActivity.Header?
+                    if (h?.fragment == "com.android.settings.device.MiuiDeviceDetailSettings") {
+                        h?.fragment = "com.android.settings.device.u"
+                    }
+                }
+            })
+        }
+
         val clsDetailSettings = XpUtils.findClass(loadPackageParam.classLoader, "com.android.settings.device.MiuiDeviceDetailSettings")
         if (clsDetailSettings != null) {
             XpUtils.findAndHookMethod("com.android.settings.device.MiuiDeviceDetailSettings", loadPackageParam.classLoader, "onInflateView", LayoutInflater::class.java, ViewGroup::class.java, Bundle::class.java, object : XC_MethodHook() {
@@ -27,7 +51,6 @@ object FuckSettings {
                     val li = sv.getChildAt(0) as LinearLayout
                     if (li.childCount == 5) {
                         val lay2 = li.getChildAt(1) as LinearLayout
-                        lay2.getChildAt(0).visibility = View.GONE
                         lay2.getChildAt(1).visibility = View.GONE
                         lay2.getChildAt(2).visibility = View.GONE
                         lay2.getChildAt(7).visibility = View.GONE
