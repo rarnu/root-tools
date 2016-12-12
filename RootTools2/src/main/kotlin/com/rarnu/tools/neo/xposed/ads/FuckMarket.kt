4fe076180d6f6ca15a014e5119ea91e09f081941
rarnu <hexiaojie@hujiang.com>
2016-12-12 11:21:58 +0800

    * fucked ads in new market 250

--- RootTools2/src/main/kotlin/com/rarnu/tools/neo/xposed/ads/FuckMarket.kt
+++ RootTools2/src/main/kotlin/com/rarnu/tools/neo/xposed/ads/FuckMarket.kt
@@ -4,6 +4,7 @@ import android.content.Loader
 import com.rarnu.tools.neo.xposed.XpUtils
 import de.robv.android.xposed.XC_MethodHook
 import de.robv.android.xposed.callbacks.XC_LoadPackage
+import java.lang.reflect.Field
 import java.util.*
 
 /**
@@ -48,7 +49,7 @@ object FuckMarket {
             })
         }
 
-        // 6.x
+        // 6.x / 240
         val clsCy = XpUtils.findClass(loadPackageParam.classLoader, "com.xiaomi.market.data.cy")
         if (clsCy != null) {
             XpUtils.findAndHookMethod("com.xiaomi.market.ui.UpdateHistoryFragmentPhone", loadPackageParam.classLoader, "a", Loader::class.java, clsCy, object : XC_MethodHook() {
@@ -89,20 +90,36 @@ object FuckMarket {
             XpUtils.findAndHookMethod("com.xiaomi.market.widget.q", loadPackageParam.classLoader, "w", ArrayList::class.java, object : XC_MethodHook() {
                 @Throws(Throwable::class)
                 override fun beforeHookedMethod(param: MethodHookParam) {
-                    var fItemType = clsQItem.getDeclaredField("aTC")
-                    val list = param.args[0] as MutableList<*>?
-                    val newList = arrayListOf<Any?>()
-                    if (list != null) {
-                        for (item in list) {
-                            val typ = fItemType?.get(item) as Enum<*>?
-                            if (typ != null) {
-                                if (typ.toString() != "RECOMMEND_APP") {
-                                    newList.add(item)
+                    var fItemType: Field? = null
+                    // 6.x 240
+                    try {
+                        fItemType = clsQItem.getDeclaredField("aTC")
+                    } catch (e: Exception) {
+
+                    }
+                    if (fItemType == null) {
+                        // 6.x 250
+                        try {
+                            fItemType = clsQItem.getDeclaredField("aVA")
+                        } catch (e: Exception) {
+
+                        }
+                    }
+                    if (fItemType != null) {
+                        val list = param.args[0] as MutableList<*>?
+                        val newList = arrayListOf<Any?>()
+                        if (list != null) {
+                            for (item in list) {
+                                val typ = fItemType.get(item) as Enum<*>?
+                                if (typ != null) {
+                                    if (typ.toString() != "RECOMMEND_APP") {
+                                        newList.add(item)
+                                    }
                                 }
                             }
                         }
+                        param.args[0] = newList
                     }
-                    param.args[0] = newList
                 }
             })
 
@@ -127,5 +144,40 @@ object FuckMarket {
             })
         }
 
+        // 6.x 250
+        val clsCz = XpUtils.findClass(loadPackageParam.classLoader, "com.xiaomi.market.data.cz")
+        if (clsCz != null) {
+            XpUtils.findAndHookMethod("com.xiaomi.market.ui.UpdateHistoryFragmentPhone", loadPackageParam.classLoader, "a", Loader::class.java, clsCz, object : XC_MethodHook() {
+                @Throws(Throwable::class)
+                override fun beforeHookedMethod(param: MethodHookParam) {
+                    val loader = param.args[0] as Loader<*>
+                    val list = param.args[1]
+                    when (loader.id) {
+                        1 -> {
+                            val fATY = list.javaClass.getDeclaredField("atY")
+                            val l = fATY.get(list) as MutableList<*>?
+                            l?.clear()
+                        }
+                        2 -> {
+                            val fATP = list.javaClass.getDeclaredField("atP")
+                            val l = fATP.get(list) as MutableList<*>?
+                            l?.clear()
+                        }
+                    }
+                }
+            })
+            XpUtils.findAndHookMethod("com.xiaomi.market.ui.UpdateAppsFragmentPhone", loadPackageParam.classLoader, "a", Loader::class.java, clsCz, object : XC_MethodHook() {
+                @Throws(Throwable::class)
+                override fun beforeHookedMethod(param: MethodHookParam) {
+                    val loader = param.args[0] as Loader<*>
+                    if (loader.id == 1) {
+                        val list = param.args[1]
+                        val fATP = list.javaClass.getDeclaredField("atP")
+                        val l = fATP.get(list) as MutableList<*>?
+                        l?.clear()
+                    }
+                }
+            })
+        }
     }
 }
